497aa0756e0320f40bc62b70978b0f04cc2c4779
kailashnamata8877@gmail.com <kailashnamata2000@gmail.com>
2024-10-14 09:46:14 +0000

    animation of book tiles added

--- bookshop/webapp/controller/Homepage.controller.js
+++ bookshop/webapp/controller/Homepage.controller.js
@@ -1,21 +1,47 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+	"sap/ui/model/FilterOperator",
+	"sap/m/SuggestionItem",
+	"sap/ui/model/json/JSONModel",
 ],
-function (Controller, Filter, FilterOperator) {
+function (Controller, Filter, FilterOperator, SuggestionItem, JSONModel) {
     "use strict";
 
     return Controller.extend("bookshop.controller.Homepage", {
         onInit: function () {
-
+			this.getView().byId("searchField").setEnableSuggestions(false);
         },
+		onAfterRendering: function(){
+			var aFilters = [];
+			var filter = new Filter("type", FilterOperator.Contains, "New Arrival");
+			aFilters.push(filter);
+
+			// update list binding
+			var oList = this.byId("gridList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilters, "Application");
+		},
 
         onTabSelection: function(oEvent) {
+			var tabText = oEvent.getSource().oSelectedItem.mProperties.text;
             this.getView().byId("tabTitle").setText(oEvent.getSource().oSelectedItem.mProperties.text);
+			// add filter for search
+			var aFilters = [];
+			if (tabText && tabText.length > 0) {
+				var filter = new Filter("type", FilterOperator.Contains, tabText);
+				aFilters.push(filter);
+			}
+
+			// update list binding
+			var oList = this.byId("gridList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilters, "Application");
+			this.getView().getModel().setProperty("/tabBooksData", aFilters);
         },
 
         onSearch: function (oEvent) {
+			this.getView().byId("searchField").setEnableSuggestions(true);
 			// add filter for search
 			var aFilters = [];
 			var sQuery = oEvent.getSource().getValue();
@@ -28,6 +54,26 @@ function (Controller, Filter, FilterOperator) {
 			var oList = this.byId("gridList");
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(aFilters, "Application");
+		},
+
+		onSuggest: function (event) {
+			
+			this.oSF = this.getView().byId("searchField");
+			var sValue = event.getParameter("suggestValue"),
+				aFilters = [];
+			if (sValue) {
+				aFilters = [
+					new Filter([
+						new Filter("title", function (sText) {
+							return (sText || "").toUpperCase().indexOf(sValue.toUpperCase()) > -1;
+						})
+					], false)
+				];
+			}
+
+			this.oSF.getBinding("suggestionItems").filter(aFilters);
+			this.oSF.suggest();
+			this.getView().byId("searchField").setEnableSuggestions(false);
 		}
     });
 });
